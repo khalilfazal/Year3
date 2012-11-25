89cf3143e66e881accef740bed9bf7a5b893b782
Khalil Fazal <khalil.fazal.0@gmail.com>
2012-11-24 19:55:15 -0500

    Added comments for each sfs_* function

--- os/File System/fileSystem.h
+++ os/File System/fileSystem.h
@@ -10,12 +10,16 @@
 // Opens a file descriptor to the file.
 int sfs_open(char* pathname);
 
+// Copies data stored in a regular file into a specified memory pointer
 int sfs_read(int fd, int start, int length, char* mem_pointer);
 
+// Writes data stored in a memory location into a file
 int sfs_write(int fd, int start, int length, char* mem_pointer);
 
+// Reads a directory's contents into a memory pointer
 int sfs_readdir(int fd, char* mem_pointer);
 
+// Closes a file descriptor
 int sfs_close(int fd);
 
 // Deletes a file.
@@ -30,4 +34,5 @@ int sfs_getsize(char* pathname);
 // Gets the type of a file.
 int sfs_gettype(char* pathname);
 
+// Initializes the file system
 int sfs_initialize(int erase);
